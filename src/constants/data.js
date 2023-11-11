60ae51b91b97d3bfceb50037b61930cf6063427b
Hatice-Altun <van.altun@outlook.com>
2023-11-11 16:46:34 +0100

    Skills Component

---
+++ src/constants/data.js
@@ -0,0 +1,21 @@
+// skills
+const skills = [
+    {
+        title: "UI/UX DESIGN",
+        value: "80%"
+    },
+    {
+        title: "WEB DEVELOPMENT",
+        value: "95%"
+    },
+    {
+        title: "APP DEVELOPMENT",
+        value: "95%"
+    },
+    {
+        title: "MARKETING",
+        value: "75%"
+    },
+];
+
+export default {skills};
---
+++ src/constants/images.js
@@ -0,0 +1,6 @@
+import skills_bg from "../assets/skills-bg.jpg";
+
+
+export default{
+    skills_bg,
+};
